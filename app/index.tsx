2c814c964b46f78c1ab21cd7acfbe892cdb0511e
Shorunke999 <134306919+Shorunke999@users.noreply.github.com>
2025-09-01 04:15:02 -0700

    work om react native

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.view}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/login" style={styles.navButton}> Login Page </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    view: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    navButton: {
+      backgroundColor: "blue",
+      borderRadius: 5,
+      width: 100,
+      height: 20,
+      textAlign: "center",
+      color: "white",
+    }
+  }
+);
